c778bfaeb89f33af627d07f7a7bc96ae1068be76
Mahmoud Hany <kokxmax2005@gmail.com>
2026-09-09 20:57:40 +0300

    Add patient medical history feature and views

--- obj/Debug/net10.0/ClinicManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net10.0/ClinicManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03e271461ad97de5c9d3bed180a05265d10aedb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdda3f541c25a0115f6cc43154c291b3532b8e28")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
