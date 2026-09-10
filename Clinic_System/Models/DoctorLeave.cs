6bb4b865d82986e5ae0c37c0b5af966f5156184b
Omar abdeltawab <omarabdeltawab100@gmail.com>
2026-09-10 18:50:59 +0300

    Files of Remain part from Project added from Omar

---
+++ Clinic_System/Models/DoctorLeave.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Clinic_System.Models
+{
+    public class DoctorLeave
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int DoctorId { get; set; }
+
+        public DateTime LeaveDate { get; set; }
+
+        public string Reason { get; set; } = "";
+    }
+}
